f72f537f7415201ed85a5f1352e22304de6f8294
kian <kian@horsefucker.org>
2021-01-16 20:40:03 -0800

    Implement ENS domains in top holder list
    This required the use of an ENS subgraph. Currently I'm using the public
    one, but when my personal one finishes indexing i'll swap over.
    
    Also did some cleanup and replaced list with css grid

--- main.js
+++ main.js
@@ -14,26 +14,26 @@ if (ethereum) {
 
 const isEthereumAvailable = () => Boolean(ethereum);
 const isMetaMaskConnected = async () => await provider.listAccounts()
-    .then((accounts) => accounts.length > 0);
+    .then(accounts => accounts.length > 0);
 
 const getKiancoinContract = async () => await new ethers.Contract(kianAddress, kianAbi, provider);
 const getUserAddress = async() => await provider.getSigner().getAddress();
-const getBalanceForAddress = async (address) => await getKiancoinContract()
-    .then((contract) => contract.balanceOf(address))
-    .then((balance) => Number(balance / 1000000000000000000))
-    .then((balance) => balance.toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 18 }));
+const getBalanceForAddress = async address => await getKiancoinContract()
+    .then(contract => contract.balanceOf(address))
+    .then(balance => Number(balance / 1000000000000000000))
+    .then(balance => balance.toLocaleString(undefined, { minimumFractionDigits: 1, maximumFractionDigits: 18 }));
 
-const getUniswapContract = async (address) => await new ethers.Contract(address, uniswapAbi, provider);
+const getUniswapContract = async address => await new ethers.Contract(address, uniswapAbi, provider);
 const getKiancoinEthPrice = async () => await getUniswapContract(uniswapKiancoinAddress)
-    .then((contract) => contract.getReserves())
-    .then((reserves) => Number(reserves._reserve0) / Number(reserves._reserve1));
+    .then(contract => contract.getReserves())
+    .then(reserves => Number(reserves._reserve0) / Number(reserves._reserve1));
 const getEthUsdPrice = async () => await getUniswapContract(uniswapUsdcAddress)
-    .then((contract) => contract.getReserves())
-    .then((reserves) => Number(reserves._reserve0) / Number(reserves._reserve1) * 1e12); // times 10^12 because usdc only has 6 decimals
+    .then(contract => contract.getReserves())
+    .then(reserves => Number(reserves._reserve0) / Number(reserves._reserve1) * 1e12); // times 10^12 because usdc only has 6 decimals
 
 // returns price as { eth: kiancoin/eth price, usd: kiancoin/usd price }
 const getPricesFromUniswap = async () => Promise.all([getKiancoinEthPrice(), getEthUsdPrice()])
-    .then((res) => {
+    .then(res => {
         return {"eth": res[0], "usd": res[1] / res[0]};
     });
 
@@ -45,7 +45,7 @@ const connectWallet = async () => {
 };
 
 const getBalance = async () => {
-    return await getUserAddress().then((addr) => getBalanceForAddress(addr));
+    return await getUserAddress().then(addr => getBalanceForAddress(addr));
 };
 
 const setBalance = async (kiancoinBalance, kiancoinPrice) => {
@@ -53,7 +53,7 @@ const setBalance = async (kiancoinBalance, kiancoinPrice) => {
     document.getElementById("balance").innerHTML = kiancoinBalance + " kiancoin ($" + amountInUSD + ")";
 };
 
-const setPrice = async (kiancoinPrice) => {
+const setPrice = async kiancoinPrice => {
     document.getElementById("price").style.display = "initial";
 
     let price = kiancoinPrice.usd.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
@@ -72,16 +72,43 @@ const addToMetaMask = () => ethereum.request({
     }
 });
 
-const queryGraph = async () => {
+const queryKiancoinSubgraph = async () => {
+    // return format looks like
+    // {"data":{"transfers":[{"from":"0x0000000000000000000000000000000000000000","to":"0x5295b474f3a0bb39418456c96d6fcf13901a4aa1","value":"1000000000000000000"},{"from":"0x5295b474f3a0bb39418456c96d6fcf13901a4aa1","to":"0xbdb6eb461e9602ab64ca5e805a3906dbc1095250","value":"100000000000000000"},{"from":"0x5295b474f3a0bb39418456c96d6fcf13901a4aa1","to":"0x73bd1162e9da1b551d4601acf83158f3ac2247a9","value":"100000000000000000"}]}};
     const endpoint = "https://graph.wizwar.net/subgraphs/id/QmZc1R7j7gu3DZvQ87EPUEibUh6sYTdpLq9u5xDSrsGXog";
     const query = `{
         transfers {
+            id
             from
             to
             value
         }
     }`;
 
+    return queryGraph(endpoint, query);
+};
+
+const queryEnsSubgraph = async addresses => {
+    // return format looks like
+    // {"data":{"addr_0x08b4249c79215fdd6a048af43e9ff86b9ee6be2e":[],"addr_0x122fed718c784ad25e2a5ab351f034fc57512a48":[],"addr_0x4c71526d3f7b112aaa4c0af173a6a47ab69d54cd":[],"addr_0x5295b474f3a0bb39418456c96d6fcf13901a4aa1":[{"name":"kian.eth"}],"addr_0x73bd1162e9da1b551d4601acf83158f3ac2247a9":[],"addr_0x969b81bba3a3eae0fccc78f8b64f012fd823a912":[],"addr_0xbc31fda98ed7cf6daaf50327d922b78cbc486d23":[],"addr_0xbdb6eb461e9602ab64ca5e805a3906dbc1095250":[],"addr_0xe3a2b4da15130c7830862e945a2383f0a29b8bc8":[{"name":"isnowglobal.eth"}],"addr_0xfc6f9a3c0fe29423ca6b974109817a323998f762":[{"name":"travisformayor.eth"}],"addr_0xff82289231128fc6f185137f445d34e30d036c98":[]}}
+
+    //const endpoint = "https://graph.wizwar.net/subgraphs/id/Qmb5arRTXt2DJCPakb8iptE5mhVwNVZR5ZZR5Sm3QhvZa8";
+    // while i wait for ^ to finish syncing...
+    const endpoint = "https://api.thegraph.com/subgraphs/name/ensdomains/ens";
+    const query = `{
+        ${addresses.map(addr => {
+            return `
+                addr_${addr}: domains(where:{owner:"${addr.toLowerCase()}"}) {
+                    name
+                }
+            `;
+        })}
+    }`;
+
+    return queryGraph(endpoint, query);
+};
+
+const queryGraph = async (endpoint, query) => {
     return await fetch(endpoint, {
         method: 'POST',
         headers: {
@@ -91,47 +118,51 @@ const queryGraph = async () => {
         body: JSON.stringify({query})
     })
     .then(r => r.json())
-    .catch((error) => {
+    .catch(error => {
         console.error('Error while querying Graph node:', error);
     });
 };
 
-const populateTopHoldersList = async (queryResp) => {
+const populateTopHoldersList = async queryResp => {
     // This is actually a bit tricky. I want to make an ordrered list of the holders of kiancoin,
     // but the erc20 contract doesn't give us a way to reverse-lookup who the holders are.
     // So what we do is constuct the current state from the past transaction history.
 
-    let userAddress = await getUserAddress().catch((error) => {
+    let userAddress = await getUserAddress().catch(error => {
         console.error('Error while retrieving user address:', error);
         return "";
     });
 
     try {
-        var transfers = queryResp.data.transfers;
-        var balances = await calcBalances(queryResp.data.transfers);   
+        const transfers = queryResp.data.transfers;
+        const balances = await calcBalances(queryResp.data.transfers);
+        const sortedBalances = Object.entries(balances).sort(([,a],[,b]) => (a > b) ? -1 : ((a < b) ? 1 : 0));
 
-        for (const addr in balances) {
-            if (addr == "0x0000000000000000000000000000000000000000")
+        for (const i in sortedBalances) {
+            //await new Promise(r => setTimeout(r, Math.random() * 10));
+
+            if (sortedBalances[i][0] == "0x0000000000000000000000000000000000000000")
                 continue;
-            if (addr == "0x636483cb4e3e09e4a8e9d7f618a7f544579cc38c")
+            if (sortedBalances[i][0] == "0x636483cb4e3e09e4a8e9d7f618a7f544579cc38c") // uniswap contract
                 continue;
 
-            let balance = ethers.utils.formatEther(balances[addr]);
+            const addrDiv = document.createElement('div');
+            addrDiv.className = "address";
+            addrDiv.id = "addr_" + sortedBalances[i][0];
+            addrDiv.appendChild(document.createTextNode(sortedBalances[i][0]));
 
-            let you = "";
-            if (addr.toLowerCase() == userAddress.toLowerCase())
-                you = " (you)";
+            const balance = ethers.utils.formatEther(sortedBalances[i][1]);
+            const balDiv = document.createElement('div');
+            balDiv.className = "balance bal_" + sortedBalances[i][0];
+            balDiv.appendChild(document.createTextNode(balance));
 
-            var entry = addr + you + ": " + balance  + " kiancoins";
-            var li = document.createElement('li');
-            li.appendChild(document.createTextNode(entry));
-            li.style.order = balances[addr] / 10000000000n;
-            document.getElementById("top-holders").appendChild(li);
+            document.getElementById("top-holders").appendChild(addrDiv);
+            document.getElementById("top-holders").appendChild(balDiv);
         }
     } catch (error) {
         // wait before printing 'error' so it feels like the webpage actually tried lol
         await new Promise(r => setTimeout(r, 1000));
-        console.warn("Unable to parse graph query response", error);
+        console.warn("Unable to parse kiancoin subgraph query response", error);
         console.warn("Full query resp: " + JSON.stringify(queryResp));
         document.getElementById("top-holders-loading").innerHTML = "Unable to fetch list of holders.";
         document.getElementById("top-holders-loading").style.color = "salmon";
@@ -140,6 +171,27 @@ const populateTopHoldersList = async (queryResp) => {
     }
 
     document.getElementById("top-holders-loading").style.display = "none";
+    document.getElementById("top-holders-loading-blank").style.display = "none";
+
+};
+
+const replaceAddrWithENS = async () => {
+    try {
+        const addrs = [...document.getElementsByClassName("address")].map(addr => addr.innerText);
+        const ensResult = await queryEnsSubgraph(addrs);
+        //console.log("ensResult: " + JSON.stringify(ensResult));
+
+        // given ens, replace address w ENS
+        [...document.getElementsByClassName("address")].forEach(async addrNode => {
+            var addr_ = "addr_" + addrNode.innerText;
+            if (ensResult.data[addr_].length > 0) {
+                document.getElementById(addr_).innerHTML = ensResult.data[addr_][0].name;
+
+            }
+        });
+    } catch (error) {
+        console.warn("Unable to parse ENS subgraph query response", error);
+    }
 };
 
 // called when we're connected and ready to query metamask
@@ -147,7 +199,7 @@ const _initialize = async () => {
     // async get balance
     // async get price (in both eth and usd)
     // when both balance+price are retrieved, populate balance and price info
-    Promise.all([getBalance(), getPricesFromUniswap()]).then((res) => {
+    Promise.all([getBalance(), getPricesFromUniswap()]).then(res => {
         setBalance(...res);
         setPrice(res[1]);
     });
@@ -155,7 +207,7 @@ const _initialize = async () => {
 
 const initialize = async () => {
     // Asynchronously start populating the list of top kiancoin holders
-    queryGraph().then((queryResp) => populateTopHoldersList(queryResp));
+    queryKiancoinSubgraph().then(queryResp => populateTopHoldersList(queryResp)).then(replaceAddrWithENS);
 
     // Start populating account balance
     if (!isEthereumAvailable()) {
@@ -164,7 +216,7 @@ const initialize = async () => {
     } else {
         document.getElementById("addToMetamask_link").addEventListener("click", addToMetaMask);
         document.getElementById("addToMetaMask").style.display = "initial";
-        await isMetaMaskConnected().then((connected) => {
+        await isMetaMaskConnected().then(connected => {
             if (connected) {
                 _initialize();
             } else {
